4223a9410959e2a52226bcb1363eb2ee4c92529c
Rahul Chouhan <78010526+Rahulchouhan100@users.noreply.github.com>
2022-06-24 11:56:18 +0530

    Add files via upload

---
+++ clock/clock.js
@@ -0,0 +1,50 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle');
+
+const days = ["sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const months = ["Jan","Feb","Mar","Apr","May","Jun","July","Aug","sep","Oct","Nov","Dec"]
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.InnerHtml = 'Dark mode'
+    }else{
+        html.classList.add('dark')
+        e.target.InnerHtml ='Light mode'
+    }
+})
+// console.log(hourEl);
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours()
+    const hourForClock = hours % 12;
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds();
+    const ampm = hours >= 12 ? 'Pm' : 'Am';
+
+    hourEl.style.transform = `translate(-50%,-100%) rotate(${scale(hourForClock, 0 ,11,0,360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0 ,59,0,360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0 ,59,0,360)}deg)`
+
+
+    timeEl.innerHTML =`${hourForClock}:${minutes < 10 ? `0${minutes}` :minutes}  ${ampm}`
+    dateEl.innerHTML =`${days[day] }, ${months[month]} <span class ="circle">${date}</span>`
+    
+
+}    
+
+const scale = (num, in_min , in_max , out_min ,out_max) =>{
+    return (num - in_min) * (out_max- out_min) / (in_max - in_min) + out_min;
+}
+setTime();
+setInterval(setTime,1000)
+
